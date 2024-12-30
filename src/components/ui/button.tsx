04f689b824598ce3344ad12e50b0ed4a4e5db68b
brianlapp <brian.lapp@gmail.com>
2024-12-30 14:26:00 -0500

    new xxl button added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         xl: "h-14 rounded-md px-10 text-lg",
+        xxl: "h-17 rounded-md px-14 text-3xl",
         icon: "h-10 w-10",
       },
     },
